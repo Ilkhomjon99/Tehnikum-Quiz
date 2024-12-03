368a8733b65abb6d6f33856d98b4129e60debacb
Ilkhomjon99 <ilhomjonalyakupov2672@gmail.com>
2024-12-03 14:21:39 +0500

    .

--- tehnikum-quiz-main/src/pages/StepTwo.jsx
+++ tehnikum-quiz-main/src/pages/StepTwo.jsx
@@ -1,22 +1,23 @@
 import { Button, Input } from "../components";
 
 export const StepTwo = () => {
+  const answers = ['Ваш ответ', 'Ваш ответ', 'Ваш ответ', 'Ваш ответ']; // Массив вынесен
+
   return (
     <div className="variants-quiz">
       <div className="question">
         <h2>2. Занимательный вопрос</h2>
         <ul className="variants">
-          {/* Вынести массив из JSX */}
-          {['Ваш ответ', 'Ваш ответ', 'Ваш ответ', 'Ваш ответ'].map((ans, index) => {
-            return <li className="variant-wrapper" key={index}>
+          {answers.map((ans, index) => (
+            <li className="variant-wrapper" key={index}>
               <Input
                 type="radio"
-                name={`variant-${index}`}
+                name="variant" // Сделано общее имя для группы радиокнопок
                 id={`variant-${index}`}
               />
               <label htmlFor={`variant-${index}`}>{ans}</label>
             </li>
-          })}
+          ))}
         </ul>
         <Button type="button" id="next-btn" text="Далее" disabled />
       </div>
